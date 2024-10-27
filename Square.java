d2b7c8764a6b82099a5d6cf98ae0316d8a97bc70
chase930 <mchase7512@gmail.com>
2024-10-26 23:18:09 -0400

    Adding Square.java

---
+++ Square.java
@@ -0,0 +1,28 @@
+public class Square {
+	Point A;
+	Point B;
+	Point C;
+	Point D;
+	
+	public Square(Point A, Point B, Point C, Point D) {
+		this.A = A;
+		this.B = B;
+		this.C = C;
+		this.D = D;
+	}
+	
+	public double perimeter() {
+		return A.distance(B) + B.distance(C) + C.distance(D) + D.distance(A);
+	}
+	
+	public boolean isEquilateral() {
+		double dAB = A.distance(B);
+		double dBC = B.distance(C);
+		double dCD = C.distance(D);
+		double dDA = D.distance(A);
+		double dAC = A.distance(C); 
+		double dBD = B.distance(D); 
+		
+		return (dAB == dBC && dBC == dCD && dCD == dDA && dAC == dBD);
+	}
+}
